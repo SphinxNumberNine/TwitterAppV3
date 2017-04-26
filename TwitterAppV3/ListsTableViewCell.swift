fdf54ed6af94ce25115d8bdaf2f9a9f6e1d98b1c
Chirag <chiragv00@gmail.com>
2017-04-26 18:54:15 -0400

    Added list item Controller

---
+++ TwitterAppV3/ListsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListsTableViewCell.swift
+//  TwitterAppV3
+//
+//  Created by Chirag on 4/26/17.
+//  Copyright © 2017 Chitra Ramaswamy. All rights reserved.
+//
+
+import UIKit
+
+class ListsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
